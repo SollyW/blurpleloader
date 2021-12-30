93d2771a915fc34fb4af75b2b87f7aaa7a4ea20b
Solly Watkins <solly@sollyw.com>
2021-12-30 22:32:39 +1100

    Beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans beans

--- src/main/java/com/projectblurple/blurpleloader/BlurpleGameProvider.java
+++ src/main/java/com/projectblurple/blurpleloader/BlurpleGameProvider.java
@@ -1,6 +1,7 @@
 package com.projectblurple.blurpleloader;
 
 import net.fabricmc.loader.impl.game.minecraft.MinecraftGameProvider;
+import net.fabricmc.loader.impl.launch.FabricLauncher;
 import net.fabricmc.loader.impl.util.log.Log;
 import net.fabricmc.loader.impl.util.log.LogCategory;
 
@@ -11,8 +12,8 @@ public class BlurpleGameProvider extends MinecraftGameProvider {
     }
 
     @Override
-    public void launch(ClassLoader loader) {
+    public void initialize(FabricLauncher launcher) {
         Log.info(LogCategory.GAME_PROVIDER, "Blurple game provider loaded; updated");
-        super.launch(loader);
+        super.initialize(launcher);
     }
 }
